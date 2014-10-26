2ebf1a7c93f9ef8db895e07df79e22f665167ebf
Nikolay Blokhin <nikolay@blokhin.us>
2014-10-26 14:02:08 +0200

    Add app and local settings

--- setup.sh
+++ setup.sh
@@ -6,6 +6,4 @@ source ./env/bin/activate
 
 ./scripts/install_python_requirements.sh
 
-#cp ./src/project/local_settings.py.development ./src/project/local_settings.py
-
-#mkdir ./data/ && ./scripts/sync_db.sh
+cp ./src/project/local_settings.py.development ./src/project/local_settings.py
--- src/project/project/settings.py
+++ src/project/project/settings.py
@@ -1,30 +1,21 @@
-"""
-Django settings for project project.
+#!/usr/bin/env python
+# encoding: utf-8
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
+import os
 
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+ALLOWED_HOSTS = []
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '6e8+rwqc-rf&7l@lo17#+06mifgy612=i=od7*wv_8^$pd)h8v'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -36,6 +27,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -53,9 +46,6 @@ ROOT_URLCONF = 'project.urls'
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -63,17 +53,12 @@ DATABASES = {
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -81,3 +66,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
+try:
+    from local_settings import *
+except ImportError:
+    print 'Warning! No local_settings module found!'
+
